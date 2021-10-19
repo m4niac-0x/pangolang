fa6b6f13d65716a63557dab7eef2d13395e6194b
m4niac-0x <72668422+m4niac-0x@users.noreply.github.com>
2021-10-19 23:04:03 +0200

    Convert to package format

---
+++ pkg/pangolang.go
@@ -0,0 +1,188 @@
+package pangolang
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net"
+	"net/http"
+	"os"
+	"os/exec"
+	"path"
+	"time"
+
+	"github.com/k0kubun/go-ansi"
+	"github.com/schollz/progressbar/v3"
+)
+
+func CheckError(err error) {
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Fatal error: %s", err.Error())
+		os.Exit(1)
+	}
+}
+
+func ExecuteUnixCmd(c string, a ...string) {
+	cmd := exec.Command(c, a...)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	cmd.Stdin = os.Stdin
+	err := cmd.Run()
+	if err != nil {
+		CheckError(err)
+	}
+}
+
+func ProgressBarbasic() {
+	bar := progressbar.Default(100)
+	for i := 0; i < 100; i++ {
+		bar.Add(1)
+		time.Sleep(40 * time.Millisecond)
+	}
+}
+
+func ProgressBarCustom() {
+	doneCh := make(chan struct{})
+
+	bar := progressbar.NewOptions(1000,
+		progressbar.OptionSetWriter(ansi.NewAnsiStdout()),
+		progressbar.OptionEnableColorCodes(true),
+		progressbar.OptionShowBytes(true),
+		progressbar.OptionSetWidth(15),
+		progressbar.OptionSetDescription("[cyan][1/3][reset] Writing moshable file..."),
+		progressbar.OptionSetTheme(progressbar.Theme{
+			Saucer:        "[green]=[reset]",
+			SaucerHead:    "[green]>[reset]",
+			SaucerPadding: " ",
+			BarStart:      "[",
+			BarEnd:        "]",
+		}),
+		progressbar.OptionOnCompletion(func() {
+			doneCh <- struct{}{}
+		}),
+	)
+
+	go func() {
+		for i := 0; i < 1000; i++ {
+			bar.Add(1)
+			time.Sleep(5 * time.Millisecond)
+		}
+	}()
+
+	// got notified that progress bar is complete.
+	<-doneCh
+	fmt.Printf("\n ======= progress bar completed ==========\n")
+}
+
+func ProgressBarDownload(url string) {
+	req, _ := http.NewRequest("GET", url, nil)
+	resp, err := http.DefaultClient.Do(req)
+	CheckError(err)
+	defer resp.Body.Close()
+
+	filename := (path.Base(req.URL.Path))
+	f, _ := os.OpenFile(filename, os.O_CREATE|os.O_WRONLY, 0644)
+	defer f.Close()
+
+	bar := progressbar.DefaultBytes(
+		resp.ContentLength,
+		"downloading",
+	)
+	io.Copy(io.MultiWriter(f, bar), resp.Body)
+}
+
+func ProgressBarDownloadUnknown(url string) {
+	req, _ := http.NewRequest("GET", url, nil)
+	resp, err := http.DefaultClient.Do(req)
+	CheckError(err)
+	defer resp.Body.Close()
+
+	filename := (path.Base(req.URL.Path))
+	f, _ := os.OpenFile(filename, os.O_CREATE|os.O_WRONLY, 0644)
+	defer f.Close()
+
+	bar := progressbar.DefaultBytes(
+		-1,
+		"downloading",
+	)
+	io.Copy(io.MultiWriter(f, bar), resp.Body)
+}
+
+func GetUserInput(r string) string {
+	request := r
+	var a string
+	fmt.Printf("%s ", request)
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		a := scanner.Text()
+		// fmt.Println(r)
+		if len(a) < 1 {
+			a = "null"
+			return a
+		} else {
+			return a
+		}
+	}
+	return a
+}
+
+///////////
+// 	dir := "/path/to/dir"
+//
+// 	if !dirExist(dir) {
+// 		fmt.Println("Directory doesn't exist")
+// 	}
+// 	e, err := dirIsEmpty(dir)
+// 	checkError(err)
+// 	if !e {
+// 		fmt.Println("Directory insn't empty")
+// 	}
+// }
+//////////
+
+func DirExist(d string) bool {
+	_, err := os.Stat(d)
+	var b bool
+	if err == nil {
+		b = true
+	} else if os.IsNotExist(err) {
+		b = false
+	} else {
+		CheckError(err)
+	}
+	return b
+}
+
+func DirIsEmpty(d string) (bool, error) {
+	f, err := os.Open(d)
+	if err != nil {
+		return false, err
+	}
+	defer f.Close()
+
+	_, err = f.Readdirnames(1)
+	if err == io.EOF {
+		return true, nil
+	}
+	return false, err
+}
+
+func TcpClient() {
+
+	tcpAddr, err := net.ResolveTCPAddr("tcp4", "127.0.0.1:8000")
+	CheckError(err)
+
+	conn, err := net.DialTCP("tcp", nil, tcpAddr)
+	CheckError(err)
+
+	_, err = conn.Write([]byte("HEAD / HTTP/1.0\r\n\r\n"))
+	CheckError(err)
+
+	result, err := ioutil.ReadAll(conn)
+	CheckError(err)
+
+	fmt.Println(string(result))
+
+	os.Exit(0)
+}
